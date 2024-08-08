f1dcd4b3a740d9c6c84b075a167a883ea2ff0cd8
Aniruddha Paul <94107602+aniruddhapaul882@users.noreply.github.com>
2024-08-08 16:36:16 +0530

    Update run_inference.py

--- run_inference.py
+++ run_inference.py
@@ -1,7 +1,7 @@
-import os
 from scripts.inference import main as inference_main
 
 if __name__ == '__main__':
     
-    inference_main()
-
+    image_path = 'path_to_your_image.jpg'  # Change this to your actual image path
+    
+    inference_main(image_path)
